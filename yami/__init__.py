f3d0f7556dc125b1113257d0edff4685005c2a7e
UMEZAWA Takeshi <umezawa@utsoft.org>
2019-08-12 21:59:38 +0900

    create_app で、引数に渡した config から app を作る処理を追加した。

--- yami/__init__.py
+++ yami/__init__.py
@@ -23,7 +23,10 @@ from decimal import Decimal
 
 def create_app(test_config=None):
 	app = Flask(__name__, instance_relative_config = True)
-	app.config.from_pyfile("config.py")
+	if test_config is None:
+		app.config.from_pyfile("config.py")
+	else:
+		app.config.from_mapping(test_config)
 
 	orig = app.config["YAMI_PRICE_STEP_RULE"]
 	dcml = {}
