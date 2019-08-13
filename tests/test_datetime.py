af29e479ee3d2b7b37433b441bc583526a207ee4
UMEZAWA Takeshi <umezawa@utsoft.org>
2019-08-13 20:20:21 +0900

    aware な datetime の比較は UTC 時刻に変換してから行われるので、tzinfo まで含めた比較を行いたい場合は明示的に tzinfo を見る必要があった。

--- tests/test_datetime.py
+++ tests/test_datetime.py
@@ -1,72 +1,87 @@
 import pytest
 from yami import create_app, logic
 import tzlocal
+import pytz
 from datetime import datetime, timedelta, timezone
 
 
 def test_aware_utc_datetime():
 	src = {
 		"foo":                "bar",
-		"datetimefoo":        datetime(1900, 1, 1),
-		"datetime_foo":       datetime(1900, 2, 1),
-		"datetime_foo_local": datetime(1900, 3, 1),
-		"datetime_foolocal":  datetime(1900, 4, 1),
+		"datetimefoo":        datetime(2000, 1, 1),
+		"datetime_foo":       datetime(2000, 2, 1),
+		"datetime_foo_local": datetime(2000, 3, 1),
+		"datetime_foolocal":  datetime(2000, 4, 1),
 	}
 	expected = {
 		"foo":                "bar",
-		"datetimefoo":        datetime(1900, 1, 1),
-		"datetime_foo":       datetime(1900, 2, 1, tzinfo=timezone.utc),
-		"datetime_foo_local": datetime(1900, 3, 1, tzinfo=timezone.utc),
-		"datetime_foolocal":  datetime(1900, 4, 1, tzinfo=timezone.utc),
+		"datetimefoo":        datetime(2000, 1, 1),
+		"datetime_foo":       datetime(2000, 2, 1, tzinfo=timezone.utc),
+		"datetime_foo_local": datetime(2000, 3, 1, tzinfo=timezone.utc),
+		"datetime_foolocal":  datetime(2000, 4, 1, tzinfo=timezone.utc),
 	}
 
 	logic.aware_utc_datetime(src)
 	assert src == expected
+	for k in ("datetime_foo", "datetime_foo_local", "datetime_foolocal"):
+		assert src[k].tzinfo == timezone.utc
 
 
 def test_append_localtime(monkeypatch):
-	tz = timezone(timedelta(hours=1))
-	monkeypatch.setattr(tzlocal, "get_localzone", lambda: tz)
+	local_tz = pytz.timezone("Asia/Tokyo")
+	monkeypatch.setattr(tzlocal, "get_localzone", lambda: local_tz)
+
+	expected_tz = timezone(timedelta(hours=9))
 
 	src = {
 		"foo":                     "bar",
-		"datetimefoo":             datetime(1900, 1, 1, 0, tzinfo=timezone.utc),
-		"datetime_foo":            datetime(1900, 2, 1, 0, tzinfo=timezone.utc),
-		"datetime_foolocal":       datetime(1900, 4, 1, 0, tzinfo=timezone.utc),
+		"datetimefoo":             datetime(2000, 1, 1, 0, tzinfo=timezone.utc),
+		"datetime_foo":            datetime(2000, 2, 1, 0, tzinfo=timezone.utc),
+		"datetime_foolocal":       datetime(2000, 4, 1, 0, tzinfo=timezone.utc),
 	}
 	expected = {
 		"foo":                     "bar",
-		"datetimefoo":             datetime(1900, 1, 1, 0, tzinfo=timezone.utc),
-		"datetime_foo":            datetime(1900, 2, 1, 0, tzinfo=timezone.utc),
-		"datetime_foo_local":      datetime(1900, 2, 1, 1, tzinfo=tz),
-		"datetime_foolocal":       datetime(1900, 4, 1, 0, tzinfo=timezone.utc),
-		"datetime_foolocal_local": datetime(1900, 4, 1, 1, tzinfo=tz),
+		"datetimefoo":             datetime(2000, 1, 1, 0, tzinfo=timezone.utc),
+		"datetime_foo":            datetime(2000, 2, 1, 0, tzinfo=timezone.utc),
+		"datetime_foo_local":      datetime(2000, 2, 1, 9, tzinfo=expected_tz),
+		"datetime_foolocal":       datetime(2000, 4, 1, 0, tzinfo=timezone.utc),
+		"datetime_foolocal_local": datetime(2000, 4, 1, 9, tzinfo=expected_tz),
 	}
 
 	logic.append_localtime(src)
 	assert src == expected
+	for k in ("datetimefoo", "datetime_foo", "datetime_foolocal"):
+		assert src[k].tzinfo == timezone.utc
+	for k in ("datetime_foo_local", "datetime_foolocal_local"):
+		assert src[k].utcoffset() == timedelta(hours=9)
 
 
 def test_append_localtime_overwrite(monkeypatch):
-	tz = timezone(timedelta(hours=1))
-	monkeypatch.setattr(tzlocal, "get_localzone", lambda: tz)
+	local_tz = pytz.timezone("Asia/Tokyo")
+	monkeypatch.setattr(tzlocal, "get_localzone", lambda: local_tz)
+
+	expected_tz = timezone(timedelta(hours=9))
 
 	src = {
 		"foo":                     "bar",
-		"datetimefoo":             datetime(1900, 1, 1, 0, tzinfo=timezone.utc),
-		"datetime_foo":            datetime(1900, 2, 1, 0, tzinfo=timezone.utc),
+		"datetimefoo":             datetime(2000, 1, 1, 0, tzinfo=timezone.utc),
+		"datetime_foo":            datetime(2000, 2, 1, 0, tzinfo=timezone.utc),
 		"datetime_foo_local":      "bar",
-		"datetime_foolocal":       datetime(1900, 4, 1, 0, tzinfo=timezone.utc),
+		"datetime_foolocal":       datetime(2000, 4, 1, 0, tzinfo=timezone.utc),
 		"datetime_foolocal_local": "bar",
 	}
 	expected = {
 		"foo": "bar",
-		"datetimefoo":             datetime(1900, 1, 1, 0, tzinfo=timezone.utc),
-		"datetime_foo":            datetime(1900, 2, 1, 0, tzinfo=timezone.utc),
-		"datetime_foo_local":      datetime(1900, 2, 1, 1, tzinfo=tz),
-		"datetime_foolocal":       datetime(1900, 4, 1, 0, tzinfo=timezone.utc),
-		"datetime_foolocal_local": datetime(1900, 4, 1, 1, tzinfo=tz),
+		"datetimefoo":             datetime(2000, 1, 1, 0, tzinfo=timezone.utc),
+		"datetime_foo":            datetime(2000, 2, 1, 0, tzinfo=timezone.utc),
+		"datetime_foo_local":      datetime(2000, 2, 1, 9, tzinfo=expected_tz),
+		"datetime_foolocal":       datetime(2000, 4, 1, 0, tzinfo=timezone.utc),
+		"datetime_foolocal_local": datetime(2000, 4, 1, 9, tzinfo=expected_tz),
 	}
 
 	logic.append_localtime(src)
 	assert src == expected
+	for k in ("datetimefoo", "datetime_foo", "datetime_foolocal"):
+		assert src[k].tzinfo == timezone.utc
+	for k in ("datetime_foo_local", "datetime_foolocal_local"):
+		assert src[k].utcoffset() == timedelta(hours=9)
