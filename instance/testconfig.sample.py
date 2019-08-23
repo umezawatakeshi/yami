8e243f4aa27fc945e824c20900e013df2fbd6e09
UMEZAWA Takeshi <umezawa@utsoft.org>
2019-08-24 03:08:43 +0900

    結合テスト用のコンフィグのサンプルを追加した。

---
+++ instance/testconfig.sample.py
@@ -0,0 +1,8 @@
+# specify MYSQL_CONNECT_KWARGS for database dedicated to integration tests
+MYSQL_CONNECT_KWARGS = {
+	"host":     "localhost",
+	"port":     3306,
+	"user":     "yami",
+	"password": "password",
+	"database": "test_yami",
+}
